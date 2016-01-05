ff2e889f5b5a172bd25e36710799bed83a7706fd
Sai Pc <schand31@asu.edu>
2016-01-05 22:47:00 +0530

    Map structure base

--- Map.py
+++ Map.py
@@ -2,4 +2,12 @@ __author__ = 'saipc'
 
 class Map(object):
     # needs to keep track of two things, links to next_scene, opening_scene
-    pass
+
+    def __init__(self):
+        pass
+
+    def next_scene(self, scene):
+        pass
+
+    def opening_scene(self):
+        pass
