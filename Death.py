93068db9e0b012e8eb4cb317957966193ec2717b
Sai Pc <schand31@asu.edu>
2016-01-06 21:15:10 +0530

    Death scene added

--- Death.py
+++ Death.py
@@ -4,4 +4,25 @@ from Scene import *
 
 # death is a scene
 class Death(Scene):
-    pass
+    # death scene doesnt have choices or anything
+    # so override
+    def list_choices(self):
+        pass
+
+    def get_choice(self):
+        pass
+
+    def enter(self, name):
+        print "Oops, looks like you chose the wrong way :("
+        print "Sorry %s, but Game Over" % (name)
+        # should also print points
+        # print "You scored %d points" % points
+        exit(0)
+    # does not have a play method
+
+if __name__ == '__main__':
+    # tiny bridge test driver
+    name = raw_input('Enter your name: ')
+    death_scene = Death(None)
+    death_scene.enter(name)
+
