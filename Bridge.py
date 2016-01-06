7d05201c142128fd2be82613ba9c541f6ff345de
Sai Pc <schand31@asu.edu>
2016-01-06 20:19:51 +0530

    Bridge scene complete

--- Bridge.py
+++ Bridge.py
@@ -3,7 +3,28 @@ from Scene import *
 
 # bridge is also a scene
 class Bridge(Scene):
+
+    def __init__(self, choices):
+        super(Bridge, self).__init__(choices)
+
     def enter(self, name):
         super(Bridge, self).enter(name)
-        choices = []
 
+    def play(self):
+        while True:
+            choice = self.get_choice()
+            if "Jump" == choice:
+                print "Oops, you died"
+                break
+            elif "Cross" == choice:
+                print "Great, you crossed"
+                break
+            else:
+                print "Invalid choice"
+
+if __name__ == '__main__':
+    name = raw_input('Enter your name: ')
+    bridge_scene = Bridge(["Jump", "Cross"])
+    bridge_scene.enter(name)
+    bridge_scene.list_choices()
+    bridge_scene.play()
--- Scene.py
+++ Scene.py
@@ -1,18 +1,23 @@
 __author__ = 'saipc'
 
 class Scene(object):
+
+    def __init__(self, choices):
+        self.choices = choices
+
     def enter(self, name):
-        print 'Welcome', name, 'What do you choose?'
+        print 'Welcome %s to the %s. What do you choose?' % (name, self.__class__.__name__)
 
     # let each scene have choices to make
-    def list_choices(self, choices):
+    def list_choices(self):
         x = 1
-        for choice in choices:
+        for choice in self.choices:
             print x, '. ', choice
+            x += 1
 
     # this also belongs in the base class
     def get_choice(self):
-        choice = raw_input("Enter the choice number")
+        choice = raw_input("Enter the choice: ")
         return choice
 
 
