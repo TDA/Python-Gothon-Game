93019ffdf02b29a31cb27febd1e8202f0c92c93a
Sai Pc <schand31@asu.edu>
2016-01-06 21:24:39 +0530

    Escape pod completed

--- EscapePod.py
+++ EscapePod.py
@@ -1,7 +1,34 @@
 __author__ = 'saipc'
 
 from Scene import *
+from random import *
 
 # EscapePod is a scene
 class EscapePod(Scene):
-    pass
+
+    def __init__(self, choices = None):
+        choices = []
+        x = 'Pod' + str(randint(10, 50))
+        y = 'Pod' + str(randint(51, 100))
+        choices.extend([x, y])
+        # choice is from random
+        self.right_choice = choice(choices)
+        super(EscapePod, self).__init__(choices)
+
+    def play(self):
+        while True:
+            choice = self.get_choice()
+            if self.right_choice == choice:
+                print "Congrats, you have chosen the right pod"
+                return True
+            else:
+                print "Oops, wrong Pod, gg."
+                return False
+
+if __name__ == '__main__':
+    # tiny bridge test driver
+    name = raw_input('Enter your name: ')
+    escape_pod_scene = EscapePod()
+    escape_pod_scene.enter(name)
+    escape_pod_scene.list_choices()
+    escape_pod_scene.play()
