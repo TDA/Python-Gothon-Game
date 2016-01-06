bc43862dbb1e1d4bdffeb932aeb7831fdfe06dcc
Sai Pc <schand31@asu.edu>
2016-01-06 20:35:42 +0530

    Corridor scene done

--- Corridor.py
+++ Corridor.py
@@ -2,4 +2,22 @@ __author__ = 'saipc'
 from Scene import *
 # corridor is a scene
 class Corridor(Scene):
-    pass
+    def play(self):
+        while True:
+            choice = self.get_choice()
+            if "Left" == choice:
+                print "Well, lets proceed"
+                return False
+            elif "Right" == choice:
+                print "Great, you move to the next stage"
+                return True
+            else:
+                print "Invalid choice"
+
+if __name__ == '__main__':
+    # tiny bridge test driver
+    name = raw_input('Enter your name: ')
+    corridor_scene = Corridor(["Left", "Right"])
+    corridor_scene.enter(name)
+    corridor_scene.list_choices()
+    corridor_scene.play()
