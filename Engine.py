7b7be510f0d03df4239b6e3fde833205b56d0355
Sai Pc <schand31@asu.edu>
2016-01-07 22:13:11 +0530

    added comments

--- Engine.py
+++ Engine.py
@@ -9,3 +9,11 @@ class Engine(object):
 
     def play(self):
         pass
+
+if __name__ =='__main__':
+    # write down the logic for the game here
+    # define each stage and the next/prev
+    # according to the flowchart
+    # let each scene return the nextScene as part
+    # of play, so that we can easily move across the game
+    pass
